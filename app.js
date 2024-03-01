8cd5331d294e66d2657cdf7c919c2c40a6c1afaf
Rushikesh Phadtare <113949814+Rushi0207@users.noreply.github.com>
2024-03-01 05:34:42 +0530

    added localhost link

--- app.js
+++ app.js
@@ -815,5 +815,5 @@ app.get("/logout", (req, res) => {
 
 const port = process.env.PORT || 3005;
 app.listen(port, () => {
-  console.log(`Server started at port ${port}`);
+  console.log(`Server started at http://localhost:${port}`);
 });
